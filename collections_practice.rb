1aafda13ebdda5eb9a2a6df1d453a2920a0799fe
Wayne Buxton <waynebuxton2184@yahoo.com>
2017-12-22 23:37:31 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -40,9 +40,10 @@ end
 
 def add_s(array)
   array.each_with_index.collect do |word,index|
-    if index == 1 
+    if index == 1
       "feet"
-    else word + "s"
+    else
+      word + "s"
     #binding.pry
   end
 end
